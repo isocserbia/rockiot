583559c76ca549b19ab5338e49599ec471e33f72
cevanica <cevanica@yahoo.com>
2021-10-03 21:20:07 +0200

    on application start, populate device history if empty, to have first changes properly presented

--- rockiot/start.prod.sh
+++ rockiot/start.prod.sh
@@ -2,6 +2,6 @@
 
 export DEBUG=False
 python manage.py migrate
+python manage.py populate_history --auto
 python manage.py collectstatic
 gunicorn -c python:project.gunicorn project.wsgi:application
-
