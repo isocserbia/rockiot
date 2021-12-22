faf9d174a5275a279ad342e9fc99b66cbe88e062
Filip Parag <filip@parag.rs>
2021-12-22 17:03:15 +0100

    new ssl keys

--- ssl/generate.sh
+++ ssl/generate.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-openssl req -new -newkey rsa:4096 -nodes -keyout decazavazduh.key -out decazavazduh.csr
+# Step 1
+# Generate CSR and submit to register
+
+mkdir -p ./keys ./certs
+openssl req -new -newkey rsa:4096 -nodes -keyout ./keys/decazavazduh.key -out decazavazduh.csr
 
 # Country Name                RS
 # State or Province Name      Beogradski okrug
@@ -10,3 +14,18 @@ openssl req -new -newkey rsa:4096 -nodes -keyout decazavazduh.key -out decazavaz
 # Common Name                 *.decazavazduh.rs
 # Email Address               secretariat@isoc.rs
 # A challenge password        af5Vvn2etXhxpTTQmPBJ
+
+# Step 2
+# Download ca-bundle and public key from register
+
+# Step 3
+# Append "STAR_decazavazduh_rs.crt" to "My_CA_Bundle.ca-bundle" and rename to "./certs/fullchain.pem"
+
+# Step 4
+# Save "STAR_decazavazduh_rs.crt" as "./certs/decazavazduh.crt"
+
+# Step 5
+# Distribute ./keys and ./certs to other locations in the repository
+
+rsync -rP ./certs/ docker/nginx/ssl/certs/ --delete-after
+rsync -rP ./keys/ docker/nginx/ssl/keys/ --delete-after
